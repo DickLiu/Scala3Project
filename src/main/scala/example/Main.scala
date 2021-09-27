63282b455fd1443ba5329320727b9dfd0f3e54a9
495100061dick@gmail.com <495100061dick@gmail.com>
2021-09-27 16:57:30 +0800

    add field

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -3,9 +3,14 @@ package example
 class Rational(n: Int, d: Int):
   require(d != 0)
 
+  val number: Int = n
+  val denom: Int = d
+
   override def toString: String = s"$n/$d"
 
-  println("Created " + n + "/" + d)
+  def add(that: Rational): Rational =
+    Rational(number * that.denom + that.number * denom,
+      denom * that.denom)
 
 object Main {
   def main(args: Array[String]): Unit = {
